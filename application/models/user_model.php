afb141fef2946aecb1712ede708e4fbc2414ef88
BhoomikaC <bhoomikachoudum@gmail.com>
2018-05-27 14:42:55 +0530

    Added table_user and made username unique

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -5,7 +5,7 @@
         
         private function get_users()
         {
-            $user_results = $this->db->get('students');
+            $user_results = $this->db->get('table_students');
             //return $user_results->result();
             return $user_results;
         }
@@ -21,7 +21,6 @@
            }else{
                $db_pwd = '';
            }
-
            if(password_verify($password, $db_pwd)){
                 return $results->row(0)->student_id;
            }else{
@@ -29,7 +28,7 @@
            }
         }
 
-        public function create_user($user_data)
+        public function create_user($user_data, $login_data)
         {
             # code...
             //Password Encryption
@@ -39,12 +38,27 @@
 
             if($user_data['registration_form'] == 'student'){
                 unset($user_data['registration_form']);
-                $inserted_data = $this->db->insert('students', $user_data);
-                return $inserted_data;
+                $inserted_data = $this->db->insert('table_students', $user_data);
+                if($inserted_data)
+                {
+                    $inserted_login_data = $this->db->insert('table_users', $login_data);
+                    if($inserted_login_data)
+                        return $inserted_data;
+                    else
+                        $this->db->delete('table_students', $user_data);
+                }
+               
             }else if($user_data['registration_form'] == 'school'){
                 unset($user_data['registration_form']);
-                $inserted_data = $this->db->insert('schools', $user_data);
-                return $inserted_data;
+                $inserted_data = $this->db->insert('table_schools', $user_data);
+                 if($inserted_data)
+                {
+                    $inserted_login_data = $this->db->insert('table_users', $login_data);
+                    if($inserted_login_data)
+                        return $inserted_data;
+                    else
+                        $this->db->delete('table_schools', $user_data);
+                }
             }
         }
 
